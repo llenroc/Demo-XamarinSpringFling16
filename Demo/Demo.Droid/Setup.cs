1ea7d1e43c7d089b74e0fdd28526efe286fd60b5
oscarvr10 <vera_reyes-10@hotmail.com>
2016-05-21 19:27:40 -0500

    ok

---
+++ Demo/Demo.Droid/Setup.cs
@@ -0,0 +1,56 @@
+using Android.Content;
+using Demo.Core.Services.Message;
+using Demo.Core.Services.Network;
+using Demo.Droid.Services.Message;
+using Demo.Droid.Services.Network;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Droid.Platform;
+using MvvmCross.Droid.Shared.Presenter;
+using MvvmCross.Droid.Views;
+using MvvmCross.Platform;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Demo.Droid
+{
+    public class Setup : MvxAndroidSetup
+    {
+        public Setup(Context applicationContext)
+            : base(applicationContext)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new Core.App();
+        }
+
+        protected override IEnumerable<Assembly> AndroidViewAssemblies => new List<Assembly>(base.AndroidViewAssemblies)
+        {
+            typeof(Android.Support.Design.Widget.NavigationView).Assembly,
+            typeof(Android.Support.Design.Widget.FloatingActionButton).Assembly,
+            typeof(Android.Support.V7.Widget.Toolbar).Assembly,
+            typeof(Android.Support.V4.Widget.DrawerLayout).Assembly,
+            typeof(Android.Support.V4.View.ViewPager).Assembly
+        };
+
+        /// <summary>
+        /// This is very important to override. The default view presenter does not know how to show fragments!
+        /// </summary>
+        protected override IMvxAndroidViewPresenter CreateViewPresenter()
+        {
+            var mvxFragmentsPresenter = new MvxFragmentsPresenter(AndroidViewAssemblies);
+            Mvx.RegisterSingleton<IMvxAndroidViewPresenter>(mvxFragmentsPresenter);
+            return mvxFragmentsPresenter;
+        }
+
+        protected override void InitializeFirstChance()
+        {
+            base.InitializeFirstChance();
+
+            Mvx.RegisterSingleton<IMessageService>(() => new MessageService());
+            Mvx.RegisterSingleton<INetworkService>(() => new DroidNetworkService());
+        }
+
+    }
+}
